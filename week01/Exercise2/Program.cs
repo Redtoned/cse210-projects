8734b5ca91d623ad8cb4e116c7ded9cea557d06a
Redtoned <cerickson@byupathway.edu>
2026-07-08 00:27:12 -0600

    commit

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,34 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.WriteLine("What is your grade percentage?");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("You received an A.");
+        }
+
+        else if (grade >= 80)
+        {
+            Console.WriteLine("You received a B.");
+        }
+
+        else if (grade >= 70)
+        {
+            Console.WriteLine("You received a C.");
+        }
+
+        else if (grade >= 60)
+        {
+            Console.WriteLine("You received a D.");
+        }
+
+        else
+        {
+            Console.WriteLine("You received an F.");
+        }
     }
 }
