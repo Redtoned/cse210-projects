c86022e4fb23331a269ea50f119f37c5df00007f
Redtoned <cerickson@byupathway.edu>
2026-07-08 01:30:46 -0600

    commit

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string input;
+        int magicNumber;
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        do
+        {
+            Console.WriteLine("What is the magic number?");
+            input = Console.ReadLine();
+            magicNumber = int.Parse(input);
+
+            if (magicNumber < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber > number)
+            {
+                Console.WriteLine("Lower");
+            }
+        } while (magicNumber != number);
+
+        Console.WriteLine("You guessed the magic number!");
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("You made " + i + " guesses.");
+        }
     }
 }
