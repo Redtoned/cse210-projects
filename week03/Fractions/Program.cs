cd1704293271ed9fc9c91241c3d24d601403c6d3
Redtoned <cerickson@byupathway.edu>
2026-07-23 16:23:33 -0600

    commit

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -6,19 +6,19 @@ namespace Fractions
     {
         static void Main(string[] args)
         {
-            Fraction fraction1 = new Fraction();
+            Fraction fraction1 = new Fraction(7);
             Console.WriteLine(fraction1.GetFractionString());
             Console.WriteLine(fraction1.GetDecimalValue());
 
-            Fraction fraction2 = new Fraction(5);
+            Fraction fraction2 = new Fraction(13);
             Console.WriteLine(fraction2.GetFractionString());
             Console.WriteLine(fraction2.GetDecimalValue());
 
-            Fraction fraction3 = new Fraction(3, 4);
+            Fraction fraction3 = new Fraction(4, 7);
             Console.WriteLine(fraction3.GetFractionString());
             Console.WriteLine(fraction3.GetDecimalValue());
 
-            Fraction fraction4 = new Fraction(1, 3);
+            Fraction fraction4 = new Fraction(5, 8);
             Console.WriteLine(fraction4.GetFractionString());
             Console.WriteLine(fraction4.GetDecimalValue());
         }
